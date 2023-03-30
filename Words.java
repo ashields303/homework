e7bdb905418205e0959751981bdf61ea22d7f6d5
Alan Shields <ashields303@gmail.com>
2023-03-30 00:02:25 -0400

    Create Words.java

---
+++ Words.java
@@ -0,0 +1,20 @@
+package com.example.session;
+
+public class Words {
+	public static void main(String[] args) {
+		// declare and init words array with words
+		String[] words = {"ten", "fading", "post", "card", "thunder", "hinge", "trailing", "batting"};
+		
+		// declare and init for loop to count through all words in words array
+		for (int i = 0; i < words.length; i++) {
+			// check to see if words are at least 3 characters in length
+			if (words[i].length() > 3) {
+				// check to see if the last 3 characters of the word = "ing"
+				if(words[i].substring(words[i].length()-3).equals("ing")){
+					// print the whole word
+					System.out.println(words[i]);					
+				}
+			}
+		}
+	}
+}
